2a26de4895d6d672c461a8aaee7990a2c18da492
Brad Ziolko <t.m.m.xiv@gmail.com>
2016-11-07 16:05:51 -0600

    Added route from registratin page to index, added admin and manager home pages, fixed register layout, and added link to home in the navbar.

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Ideal' });
 });
 
-router.get('/register', function(req, res) {
-  res.render('register', { title: 'Ideal: Register' });
-});
-
 router.post('/', function(req, res) {
   console.log(req.body.username);
   console.log(req.body.password);
   res.render('home/user', { title: "Ideal: User Home" });
+});
+
+router.get('/register', function(req, res) {
+  res.render('register', { title: 'Ideal: Register' });
+});
+
+router.post('/register', function(req, res) {
+  res.render('index', { title: 'Ideal' });
 });
 
 module.exports = router;
