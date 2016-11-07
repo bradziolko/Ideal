28bb7162a750d2d136c6a25e98762858696094f2
Brad Ziolko <t.m.m.xiv@gmail.com>
2016-11-07 15:16:17 -0600

    Hideous styelsheet right now, added register page, added login form on index, added POST route to user home page.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Ideal' });
+});
+
+router.get('/register', function(req, res) {
+  res.render('register', { title: 'Ideal: Register' });
+});
+
+router.post('/', function(req, res) {
+  console.log(req.body.username);
+  console.log(req.body.password);
+  res.render('home/user', { title: "Ideal: User Home" });
 });
 
 module.exports = router;
