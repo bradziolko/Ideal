9f51b16788b98691789ad5fa63efb52a71c4cdc5
Weston Nesseler <nesselerweston@yahoo.com>
2016-11-07 19:36:59 -0600

    Added nodemailer

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 var User = require('../models/user');
 var user = new User();
+
+var smtpConfig = {
+	host: 'smtp.gmail.com',
+	port: 465,
+	secure: true,
+	auth: {
+		user: 'idealelectoral@gmail.com',
+		pass: 'verify12'
+	}
+};
+var transporter = nodemailer.createTransport(smtpConfig);
+var mailOptions = {
+	from: '"Ideal Electoral Registration" <idealelectoral@gmail>',
+	to: 'idealelectoral@gmail.com',
+	subject: 'Confirm registration',
+	text: 'test'
+};
+transporter.sendMail(mailOptions, function(error, info) {
+	if (error){
+		return console.log(error)
+	}
+	console.log('Message sent: ' + info.response);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,6 +44,7 @@ router.get('/register', function(req, res) {
 });
 
 router.post('/register', function(req, res) {
+  
   res.render('index', { title: 'Ideal' });
 });
 
