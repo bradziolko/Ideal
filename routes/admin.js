8a729d6856e8e45fcd95aa1114f9bbef26440d50
Brad Ziolko <t.m.m.xiv@gmail.com>
2016-12-04 15:46:04 -0600

    Moved createcandidate/manager to home directory, renamed managerinitial to createmanager.jade.

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/managerinitial', function(req, res) {
-  res.render('managerinitial', { title: 'Ideal Manager Creation Page' }); 
+router.get('/createmanager', function(req, res) {
+  res.render('home/createmanager', { title: 'Ideal Manager Creation Page' }); 
 });
 
 router.get('/createcandidate', function(req, res) {
-  res.render('createcandidate', { title: 'Ideal Candidate Creation Page' }); 
+  res.render('home/createcandidate', { title: 'Ideal Candidate Creation Page' }); 
 });
 
 module.exports = router;
